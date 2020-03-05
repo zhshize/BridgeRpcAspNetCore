e059b0f8c25a9a5939e8fa701f0682959edad52d
shize <joe7086543@hotmail.com>
2020-03-05 18:01:23 +0800

    Update RpcHubExtension

--- src/BridgeRpc.Core/Extension/RpcHubExtension.cs
+++ src/BridgeRpc.Core/Extension/RpcHubExtension.cs
@@ -10,26 +10,12 @@ namespace BridgeRpc.Core.Extension
     {
         public static Task<RpcResponse> RequestAsync<T>(this IRpcHub hub, string method, T data)
         {
-            if (typeof(T) == typeof(string))
-                return hub.RequestAsync(method, new JRaw("\"" + data + "\""));
+            return hub.RequestAsync(method, data);
+        }
 
-            return hub.RequestAsync(method, new JRaw(data));
-        }
-        
-        public static Task<RpcResponse> RequestAsync<T>(this IRpcHub hub, string method, T data, TimeSpan timeout)
-        {
-            if (typeof(T) == typeof(string))
-                return hub.RequestAsync(method, new JRaw("\"" + data + "\""), timeout);
-            
-            return hub.RequestAsync(method, new JRaw(data), timeout);
-        }
-        
         public static void Notify<T>(this IRpcHub hub, string method, T data)
         {
-            if (typeof(T) == typeof(string))
-                hub.Notify(method, new JRaw("\"" + data + "\""));
-            
-            hub.Notify(method, new JRaw(data));
+            hub.Notify(method, data);
         }
     }
 }
